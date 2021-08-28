b31042530d0adc1d538556241a8032f82af7254c
ricardo-sousa <rwmsousa@gmail.com>
2021-08-28 12:03:50 -0300

    estilização da página login e estrutura

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/wallet" component={ Wallet } />
+          <Route path="/" component={ Login } />
+        </Switch>
+      </BrowserRouter>
+    </div>);
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="container">
+        <h1>TrybeWallet</h1>
+        <div className="d-flex justify-content-center h-100">
+          <div className="card">
+            <div className="card-header">
+              <h3>Login</h3>
+            </div>
+            <div className="card-body">
+              <input type="text" className="form-control" placeholder="username" />
+              <input type="password" className="form-control" placeholder="password" />
+              <input type="submit" value="Entrar" className="btn float-right login_btn" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
