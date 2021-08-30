fd432ba07a6dae525104a2f5045499393d9d728d
thomasravache <thomasravache31@gmail.com>
2021-08-30 16:52:28 -0300

    Deixando app.js como componente de classe

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello!</div>
+    );
+  }
 }
 
 export default App;
