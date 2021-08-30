511848991504e7c71cf352cf15b073e499b71658
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-08-30 15:01:41 -0300

    Primeiro commit - Trybewallet :dollar:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return 
+  <div>Hello, TrybeWallet!</div>;
+  <div>Primeiro commit</div>;
 }
 
 export default App;
