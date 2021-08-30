170802de34bec94ace1c852e636bdd9a12fe87e9
JosueLobo <darksiders20005@gmail.com>
2021-08-30 15:11:51 -0300

    iniciando projeto

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 
