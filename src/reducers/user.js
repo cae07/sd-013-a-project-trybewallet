86995b45e9e373a41f5449ed7f250b5b8d7380df
Lanai Conceição <conceicao.lanai@gmail.com>
2021-08-30 15:22:42 -0300

    Criação de store e reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,26 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOADING_TYPE, SUCCESS_TYPE, ERROR_TYPE } from '../actions';
+
+const initialState = {
+  loading: false,
+  data: '',
+  error: '',
+};
+
+function reducerUser(state = initialState, action) {
+  switch (action.type) {
+  case LOADING_TYPE:
+    return { ...state, loading: true };
+
+  case SUCCESS_TYPE:
+    return { ...state, data: payload, loading: false };
+
+  case ERROR_TYPE:
+    return { ...state, error: payload, loading: false };
+
+  default:
+    return state;
+  }
+}
+
+export default reducerUser;
