b316e63cfe237bd4b52bbed7c9358a1a160866fa
Elaine Moreira <elainefelipemoreira@gmail.com>
2021-08-30 19:35:35 -0300

    Adiciona requisito 5

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class header extends React.Component {
+  render() {
+    return (
+      <header>
+        <div data-testid="email-field">{}</div>
+      </header>
+    );
+  }
+}
+export default header;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <header>
+        <div data-testid="email-field">{ email }</div>
+        <div data-testid="total-field">0</div>
+        <div data-testid="header-currency-field">BRL</div>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = ({
+  email: PropTypes.string.isRequired,
+});
+
+export default connect(mapStateToProps)(Wallet);
