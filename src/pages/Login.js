f2b86fe20b248d6feafee9032c03da7023b42168
Gustavo Pia <piagja@gmail.com>
2021-08-30 20:36:17 -0300

    feat: add disabled state to login button, add handleLogin and handleChange methods to handle inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './Login.css';
 
 class Login extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       email: '',
       password: '',
       disabledBtn: true,
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(e) {
+    e.preventDefault();
+    const { email, password } = this.state;
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+
+    const validEmailAndPassword = () => {
+      const { email, password } = this.state;
+      const passwordLength = password.length;
+      const maxPasswordLength = 6;
+
+      if (emailPattern.test(email) && passwordLength >= maxPasswordLength) {
+        return this.setState({ disabledBtn: false });
+      }
+
+      return this.setState({ disabledBtn: true });
+    };
+
+    this.setState({
+      [name]: value,
+    }, validEmailAndPassword());
   }
 
   render() {
+    const { email, password, disabledBtn } = this.state;
+
     return (
       <main className="login-container">
         <form>
@@ -20,14 +53,32 @@ class Login extends React.Component {
           <fieldset>
             <legend>Faça seu Login</legend>
             <div data-testid="email-input" className="email-input">
-              <input type="text" placeholder="Username" />
+              <input
+                name="email"
+                value={ email }
+                type="text"
+                placeholder="Username"
+                onChange={ this.handleChange }
+              />
             </div>
 
             <div data-testid="password-input" className="password-input">
-              <input type="password" placeholder="Password" />
+              <input
+                name="password"
+                value={ password }
+                type="password"
+                placeholder="Password"
+                onChange={ this.handleChange }
+              />
             </div>
 
-            <button type="submit">Entrar</button>
+            <button
+              onClick={ this.handleLogin }
+              disabled={ disabledBtn }
+              type="submit"
+            >
+              Entrar
+            </button>
           </fieldset>
 
         </form>
@@ -37,4 +88,8 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+  user: state.userReducer,
+});
+
+export default connect(mapStateToProps)(Login);
