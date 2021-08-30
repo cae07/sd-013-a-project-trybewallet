aee30e2c0f7cbe3a9a7f47a5ac7fa079ae3090a8
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-08-30 15:00:30 -0300

    Requirement 1

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,2 +1,2 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-import { GET_EXCHANGE_RATE } from '../actions/actionTypes';
+// import { GET_EXCHANGE_RATE } from '../actions/actionTypes';
