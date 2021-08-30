ce3d74a654f30c9d4c7fbefeb81502262dd13824
vdionysio <vini.dionysio@gmail.com>
2021-08-30 17:58:21 -0300

    solving req 6

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class ExpenseForm extends Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="value">
+          Valor
+          <input type="number" name="value" id="value" />
+        </label>
+        <label htmlFor="description">
+          Descrição
+          <input type="text" name="description" id="description" />
+        </label>
+        <label htmlFor="currency">
+          Moeda
+          <select name="currency" id="currency">
+            {}
+          </select>
+        </label>
+        <label htmlFor="payment">
+          Método de pagamento
+          <select name="payment" id="payment">
+            <option value="cash">Dinheiro</option>
+            <option value="creditCard">Cartão de crédito</option>
+            <option value="debitCard">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          Tag
+          <select name="tag" id="tag">
+            <option value="food">Alimentação</option>
+            <option value="leisure">Lazer</option>
+            <option value="work">Trabalho</option>
+            <option value="transport">Transporte</option>
+            <option value="health">Saúde</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default ExpenseForm;
---
+++ src/components/InputExpense.js
@@ -0,0 +1,14 @@
+// import React, { Component } from 'react';
+
+// class InputExpense extends Component {
+//   render() {
+//     return (
+//       <label htmlFor={ name }>
+//         { children }
+//         <input type={ type } name={ name } />
+//       </label>
+//     );
+//   }
+// }
+
+// export default InputExpense;
