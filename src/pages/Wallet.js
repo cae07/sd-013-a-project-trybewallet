ca2bfec75439a1d78f1af3630f19083fb0b8312e
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-30 17:03:45 -0300

    Estruturando wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -29,7 +29,13 @@ const mapStateToProps = (state) => ({
   isEditing: state.wallet.isEditing,
 });
 
+// A função do mapDispatchToProps é despachar action para a store, com a finalidade de alterar o state da aplicação
+// A função dispatch() serve para despachar uma action para o reducer.
+// Recebe como parametro uma dispatch, e retorna um objeto com chave e valor.
 const mapDispatchToProps = (dispatch) => ({
+// A chave(fetchCurrencies) é a prop do componente que vai ser envocada
+// E eu passo uma callback que vai ser o dispatch que vou realizar
+// O retorno desse disparo será o novo valor dessa chave fetchCurrencies
   fetchCurrencies: () => dispatch(getCurrencies()),
 });
 
