8f496a1d4c46224be7e63061b2e4289c02e0997d
Juliane Marques <mqs.juliane@gmail.com>
2021-08-30 16:51:38 -0300

    configuracoes iniciais parte 3

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,27 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { LOADING_TYPE, SUCCESS_TYPE, ERROR_TYPE } from '../actions';
+
+const initialState = { // provavel que troque o estado inicial
+  loading: false,
+  data: '',
+  error: '',
+};
+
+function reducerWallet(state = initialState, { type, payload }) {
+  switch (type) {
+  
+  case LOADING_TYPE:
+    return { ...state, loading: true };
+  
+  case SUCCESS_TYPE:
+    return { ...state, data: payload, loading: false };
+  
+  case ERROR_TYPE:
+    return { ...state, error: payload, loading: false };
+  
+  default:
+    return state;
+  }
+}
+
+export default reducerWallet;
