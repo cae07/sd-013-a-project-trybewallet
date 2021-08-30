98bebfc5f204918b3fa360ad0acf3c0b484d4263
ALINE <hoshinoaline@yahoo.com.br>
2021-08-30 14:56:45 -0300

    requisito 1 a 3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,3 @@
-// Coloque aqui suas action
+export const SAVE_EMAIL = 'SAVE_EMAIL';
+
+export const saveEmail = (email) => ({ type: SAVE_EMAIL, email });
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,88 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { saveEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
+
+  onSubmitForm() {
+    const { history, EmailKey } = this.props;
+    // Disparamos a nossa action através da função importada
+    // de actions.js, que apelidamos de EmailKey
+    const { email } = this.state;
+    EmailKey(email);
+    history.push('/carteira');
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const passwordMin = 6;
+    const passwordCorrect = password.length >= passwordMin;
+    const validateEmail = () => {
+      const re = /\S+@\S+\.\S+/;
+      return re.test(email);
+    };
+    return (
+      <div>
+        <input
+          type="email"
+          name="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <div>
+          <input
+            type="password"
+            data-testid="password-input"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+        </div>
+        <button
+          type="submit"
+          disabled={ !(validateEmail() && passwordCorrect) }
+          onClick={ this.onSubmitForm }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  EmailKey: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  EmailKey: (email) => dispatch(saveEmail(email)),
+}
+);
+
+export default connect(null, mapDispatchToProps)(Login);
+
+// https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+// Nesse link acima o regex significa anystring@anystring.anystring código do stackoverflow
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+// o test retorna um booleano se o email for válido com o regex da true
+// colocando a exclamação nega e daí fica falso com isso o botao ativa.
+// https://medium.com/front-end-weekly/react-tips-disable-buttons-formdata-types-for-function-6c8f23d13b64
