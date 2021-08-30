fe4b75113302ffeea6c8e8430cacc4e743c0ebd7
Breno Lopes <brenokos4@gmail.com>
2021-08-30 18:02:17 -0300

    add requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { password, email } = this.state;
+    const SIX = 6;
+
+    const evaluatorEmail = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/g
+      .test(email);
+    const enabled = evaluatorEmail && password.length >= SIX;
+
     return (
       <>
         <div>
@@ -11,6 +37,9 @@ class Login extends React.Component {
           <input
             type="email"
             placeholder="email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
             required
             data-testid="email-input"
           />
@@ -18,9 +47,17 @@ class Login extends React.Component {
             type="password"
             minLength="6"
             required
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
             data-testid="password-input"
           />
-          <button type="submit">Entrar</button>
+          <button
+            type="submit"
+            disabled={ !enabled }
+          >
+            Entrar
+          </button>
         </form>
       </>
     );
