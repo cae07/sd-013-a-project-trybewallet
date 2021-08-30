0cd6dac8f3d7177e8436be8bd26cf66cb2450a5b
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-08-30 14:59:15 -0300

    Resolve requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route path="/">
+        <Login />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>; 
+    return (
+      <fieldset>
+        <input data-testid="email-input" type="email" placeholder="Digite seu e-mail" />
+        <input data-testid="password-input" type="text" placeholder="Digite sua senha" />
+        <button type="button">Entrar</button>
+      </fieldset>
+    );
   }
 }
 
