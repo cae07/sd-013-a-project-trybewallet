bd36bc1b61415df836af4a6d397d8ac192bef811
Gustavo Pia <piagja@gmail.com>
2021-08-30 19:20:59 -0300

    feat: add userReducer and walletReducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,28 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import {
+  LOADING_ACTION,
+  LOADING_ACTION_SUCCESS,
+  LOADING_ACTION_FAIL,
+} from '../constants';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case LOADING_ACTION:
+    return { ...state, loading: true };
+
+  case LOADING_ACTION_SUCCESS:
+    return { ...state, data: action.payload, loading: false };
+
+  case LOADING_ACTION_FAIL:
+    return { ...state, data: action.payload, loading: false };
+
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,40 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import {
+  LOADING_ACTION,
+  LOADING_ACTION_SUCCESS,
+  LOADING_ACTION_FAIL,
+} from '../constants';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const walletReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case LOADING_ACTION:
+    return {
+      ...state,
+      loading: true,
+    };
+
+  case LOADING_ACTION_SUCCESS:
+    return {
+      ...state,
+      loading: false,
+      currencies: action.payload.currencies,
+      expenses: action.payload.expenses,
+    };
+
+  case LOADING_ACTION_FAIL:
+    return {
+      ...state,
+      loading: false,
+    };
+
+  default:
+    return state;
+  }
+};
+
+export default walletReducer;
