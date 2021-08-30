df9b3d15a0a18abbb726dd6c75543ab4475aa757
Gustavo Pia <piagja@gmail.com>
2021-08-30 17:36:01 -0300

    feat: add redux-thunk and compose the application with Redux Devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-const store = createStore(() => {});
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store;
