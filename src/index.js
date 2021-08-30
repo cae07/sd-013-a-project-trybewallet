8b610c580596f70c7acff3b7960d303e3743f948
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-30 14:28:50 -0300

    Estruturando projeto

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
+
+import store from './store';
+
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
