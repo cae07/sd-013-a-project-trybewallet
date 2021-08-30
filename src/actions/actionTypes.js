8f0dc64dc07a7dc6403adfa8d1147a5905312718
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-30 15:10:23 -0300

    Estruturando as actions'

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -2,9 +2,11 @@
 export const ACTION_EMAIL = 'ACTION_EMAIL';
 
 // Action Types de Wallet
+// ** Essas são para a conexão API
 export const ACTION_REQUEST_START = 'ACTION_REQUEST_START';
 export const ACTION_REQUEST_SUCCESS = 'ACTION_REQUEST_SUCCESS';
 export const ACTION_REQUEST_FAIL = 'ACTION_REQUEST_FAIL';
+// ** Essas são para as Expenses
 export const ACTION_SAVE_EXPENSE = 'ACTION_SAVE_EXPENSE';
 export const ACTION_DELETE_EXPENSE = 'ACTION_DELETE_EXPENSE';
 export const ACTION_EDIT_EXPENSE_START = 'ACTION_EDIT_EXPENSE_START';
