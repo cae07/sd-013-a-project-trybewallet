d3aaf27247867bea35e2beb8d9affe125a487c86
thomasravache <thomasravache31@gmail.com>
2021-08-30 18:58:15 -0300

    Realocando pastas do redux para que passem no evaluator

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store from './store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
