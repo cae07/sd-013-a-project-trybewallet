668b077c70f48ff72c6ce11fc20c0c9225d4b590
Elaine Moreira <elainefelipemoreira@gmail.com>
2021-08-30 19:05:05 -0300

    Adiciona requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,67 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      email: '',
+      senha: '',
+      buttonDesabled: true,
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    }, () => this.inputValidation());
+  }
+
+  inputValidation() {
+    // REGEX retirado do post: https://stackoverflow.com/questions/35788383/regex-validation-in-javascript-email
+    const emailValidationRegex = /\S+@\S+\.\S+/;
+    const magicNumber = 6;
+    const { email, senha } = this.state;
+
+    if (emailValidationRegex.test(email) && senha.length >= magicNumber) {
+      this.setState({
+        buttonDesabled: false,
+      });
+    } else {
+      this.setState({
+        buttonDesabled: true,
+      });
+    }
+  }
+
   render() {
+    const { email, senha, buttonDesabled } = this.state;
     return (
       <div>
         <div>Login</div>
         <input
-          type="email"
           data-testid="email-input"
+          type="email"
+          placeholder="Digite seu e-mail"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
         />
+
         <input
-          type="password"
-          placeholder="Insira sua senha"
           data-testid="password-input"
+          type="text"
+          placeholder="Digite sua senha"
+          name="senha"
+          value={ senha }
+          onChange={ this.handleChange }
         />
-        <button type="button">Entrar</button>
+
+        <button type="button" disabled={ buttonDesabled }>Entrar</button>
       </div>
     );
   }
