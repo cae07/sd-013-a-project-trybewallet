e7f3f7b3c7dd09083a1cbe6065c99e93840b9cfe
Felipe Lima <felipe_lima10@icloud.com>
2021-08-30 15:53:01 -0300

    resolve exercicio 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      valid: false,
+    };
+    this.validLogin = this.validLogin.bind(this);
+    this.handleForm = this.handleForm.bind(this);
+  }
+
+  handleForm({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.validLogin();
+  }
+
+  validLogin() {
+    const { email, password } = this.state;
+    const REGEX_EMAIL = /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i;
+    const REGEX_PASSWORD = 5;
+    const isValidEmail = email.match(REGEX_EMAIL) !== null;
+    const isValidPassword = password.length >= REGEX_PASSWORD;
+    if (isValidEmail === true && isValidPassword === true) {
+      this.setState({
+        valid: true,
+      });
+    }
+  }
+
   render() {
+    const { valid } = this.state;
     return (
       <>
-        <label htmlFor="login">
+        <label htmlFor="email">
           Email:
           <input
-            name="login"
-            type="text"
+            name="email"
+            type="email"
             data-testid="email-input"
+            onChange={ this.handleForm }
           />
         </label>
         <label htmlFor="password">
@@ -18,9 +53,14 @@ class Login extends React.Component {
             name="password"
             type="password"
             data-testid="password-input"
+            onChange={ this.handleForm }
           />
         </label>
-        <input type="button" value="Entrar" />
+        <Link to="/carteira">
+          {valid
+            ? <input id="entrar" type="button" value="Entrar" />
+            : <input id="entrar" type="button" value="Entrar" disabled />}
+        </Link>
       </>
     );
   }
