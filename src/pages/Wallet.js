305dad0df7671ca5bc7d72638bf3295fb6fbf2b0
FelipeNevess <felipe012neves@gmail.com>
2021-08-30 16:17:48 -0300

    requisito 6 finalizado

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,76 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.inputValue = this.inputValue.bind(this);
+    this.selectOptionMoeda = this.selectOptionMoeda.bind(this);
+    this.selectOptionPagamento = this.selectOptionPagamento.bind(this);
+    this.selectOptionTag = this.selectOptionTag.bind(this);
+
+    this.state = {};
+  }
+
+  inputValue() {
+    return (
+      <label htmlFor="valor">
+        Valor:
+        <input type="number" id="valor" />
+      </label>
+    );
+  }
+
+  imputDescription() {
+    return (
+      <label htmlFor="description">
+        Descrição:
+        <input type="description" id="valor" />
+      </label>
+    );
+  }
+
+  selectOptionMoeda() {
+    return (
+      <label htmlFor="moeda">
+        Moeda:
+        <select id="moeda">
+          <option>Trybe</option>
+        </select>
+      </label>
+    );
+  }
+
+  selectOptionPagamento() {
+    return (
+      <label htmlFor="pagamento">
+        Método de pagamento:
+        <select id="pagamento">
+          <option>Dinheiro</option>
+          <option>Cartão de crédito</option>
+          <option>Cartão de débito</option>
+        </select>
+      </label>
+    );
+  }
+
+  selectOptionTag() {
+    return (
+      <label htmlFor="tag">
+        Tag:
+        <select id="tag">
+          <option>Alimentação</option>
+          <option>Lazer</option>
+          <option>Trabalho</option>
+          <option>Transporte</option>
+          <option>Saúde</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     const { stateEmail } = this.props;
     return (
@@ -11,10 +80,23 @@ class Wallet extends React.Component {
           <p data-testid="total-field">{`Despesa: ${0}`}</p>
           <p data-testid="header-currency-field">BRL</p>
         </header>
+        <form>
+          <fieldset>
+            {this.inputValue()}
+            {this.imputDescription()}
+            {this.selectOptionMoeda()}
+            {this.selectOptionPagamento()}
+            {this.selectOptionTag()}
+          </fieldset>
+        </form>
       </div>
     );
   }
 }
+
+Wallet.propTypes = {
+  stateEmail: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = (state) => ({
   stateEmail: state.user.email,
