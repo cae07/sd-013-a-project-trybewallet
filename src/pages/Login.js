179532047dddd1b58d338ca82090ff86837aae00
Breno Lopes <brenokos4@gmail.com>
2021-08-30 16:58:45 -0300

    add requisito 1!

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <div>
+          Login
+        </div>
+        <form>
+          <input
+            type="email"
+            placeholder="email"
+            required
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            minLength="6"
+            required
+            data-testid="password-input"
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </>
+    );
   }
 }
 
