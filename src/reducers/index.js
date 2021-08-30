76c61176db43d85231da0c91986dbb23346c33df
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-08-30 19:57:20 -0300

    Iniciando Projeto Trybe Wallet

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,4 +2,4 @@
 // import wallet from './wallet';
 
 // Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado globa
