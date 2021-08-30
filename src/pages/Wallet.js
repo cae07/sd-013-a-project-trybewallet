a2c567abb206f5a0bd4a8c3765cb55c267c58540
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-30 17:11:41 -0300

    Criando Header no wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 // Renomeio a fetchCurrencies pois foi precisar do nome dela para ser chave do meu mapDispatchToProps
 import { fetchCurrencies as getCurrencies } from '../actions';
 
-// Imposto os componentes para renderizar o Wallet
+// Importo os componentes para renderizar o Wallet
 import { ExpensesTable, Header, NewExpenseForm, EditExpenseForm } from '../components';
 
 import '../styles/Wallet.css';
@@ -13,15 +13,19 @@ import '../styles/Wallet.css';
 class Wallet extends Component {
   // O componentDidMount é chamado imediatamente após o componente ser renderizado
   componentDidMount() {
-    // Desconstroi o fetchCurrencies que está na props porque foi passado como chave no mapDispatchToProps, com o valor de disparo da action getCurrencies
+    // Desconstroi o fetchCurrencies que está na props porque foi passado como chave no mapDispatchToProps, com o valor de disparo da action fetchCurrencies que aqui o nome dela é getCurrencies, conforme explicação na linha 5
     const { fetchCurrencies } = this.props;
-    fetchCurrencies();
+
+    fetchCurrencies(); // Executo a action que dispara a busca pelas moedas na API
   }
 
   render() {
+    // isEditing é uma chave da state que é controlada conforme é executada uma action de editar ou não uma expense.
     const { isEditing } = this.props;
+
     return (
       <main className="wallet-main">
+        {/* Renderiza o Header na página do Wallet */}
         <Header />
         { isEditing ? <EditExpenseForm /> : <NewExpenseForm />}
         <ExpensesTable />
