915814b3b06ec331a26d1736506e7daed5cdb7f0
DouglasDrozda <lowdouglas9712@gmail.com>
2021-08-30 18:30:55 -0300

    feito requisito 5 e 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,108 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import trybeLogo from '../images/trybeLogo.png';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  inputValue() {
+    return (
+      <label htmlFor="value">
+        Valor:
+        <input id="value" className="valueMoeda" type="number" />
+      </label>
+    );
+  }
+
+  inputDescription() {
+    return (
+      <label htmlFor="description">
+        Descrição da despesa:
+        <input id="description" type="text" />
+      </label>
+    );
+  }
+
+  inputSelect() {
+    return (
+      <label htmlFor="moeda">
+        Moeda:
+        <select className="valueMoeda" id="moeda">
+          <option>test</option>
+        </select>
+      </label>
+    );
+  }
+
+  inputSelectPaymentMethod() {
+    return (
+      <label htmlFor="paymentMethod">
+        Método de pagamento:
+        <select id="paymentMethod">
+          <option>Dinheiro</option>
+          <option>Cartão de crédito</option>
+          <option>Cartão de débito</option>
+        </select>
+      </label>
+    );
+  }
+
+  inputSelectDespesa() {
+    return (
+      <label htmlFor="despesa">
+        Tag:
+        <select id="despesa">
+          <option>Alimentação</option>
+          <option>Lazer</option>
+          <option>Trabalho</option>
+          <option>Transporte</option>
+          <option>Saúde</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <header className="header-wallet-contain">
+          <div className="header-wallet">
+            <img src={ trybeLogo } alt="trybeLogo" width="150px" height="40px" />
+            <div className="wallet-email">
+              <p data-testid="email-field" className="email">
+                { `Email: ${email}` }
+              </p>
+              <p data-testid="total-field">{`Despesa total: ${0}`}</p>
+              <p data-testid="header-currency-field">BRL</p>
+            </div>
+          </div>
+        </header>
+        <form className="contain-forms-wallet">
+          {this.inputValue()}
+          {this.inputSelect()}
+          {this.inputSelectPaymentMethod()}
+          {this.inputSelectDespesa()}
+          {this.inputDescription()}
+          <button type="button">Adicionar despesa</button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,20 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { ENTER_EMAIL } from '../actions/index';
+
+const INITIAL_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case ENTER_EMAIL:
+    return ({
+      ...state, email: action.email,
+    });
+  default: return state;
+  }
+};
+
+export default wallet;
