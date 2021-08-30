87d284fa58c157f88bab9b3ac1de028c20bcafd8
pedroalles <pedroalles@gmail.com>
2021-08-30 15:34:28 -0300

    Realizado requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route path="/" exact component={ Login } />
+    </Switch>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input data-testid="email-input" type="text" />
+          <input data-testid="password-input" type="text" />
+          <button type="submit"> Entrar </button>
+        </form>
+      </div>
+    );
   }
 }
 
