1396387921dfa2c91e9e37047486f057d3c6d495
FelipeNevess <felipe012neves@gmail.com>
2021-08-30 15:03:28 -0300

    requisito 1 finalizado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,80 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleOnChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick() {
+    const { history } = this.props;
+
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <form>
+          <fieldset>
+            <label htmlFor="email">
+              Email:
+              <input
+                type="text"
+                value={ email }
+                onChange={ this.handleOnChange }
+                id="email"
+                placeholder="cafe@trybe.com"
+                data-testid="email-input"
+                name="email"
+              />
+            </label>
+
+            <label htmlFor="password">
+              Senha:
+              <input
+                type="password"
+                value={ password }
+                onChange={ this.handleOnChange }
+                id="password"
+                placeholder="password"
+                data-testid="password-input"
+                name="password"
+              />
+            </label>
+
+            <button
+              type="button"
+              onClick={ this.handleClick }
+            >
+              Entrar
+            </button>
+          </fieldset>
+        </form>
+      </div>
+    );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
