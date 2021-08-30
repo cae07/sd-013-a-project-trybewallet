1c2207cbaad05069654ee0c847a0f86a67790921
Thiago <thiagoc.carboneri@gmail.com>
2021-08-30 15:00:10 -0300

    criaçao input login, senha e o botao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,51 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <main>
+        <form>
+          <div
+            className="email"
+          >
+            <label
+              htmlFor="email"
+            >
+              Email
+              <input
+                data-testid="email-input"
+                type="text"
+                id="email"
+                value=""
+                placeholder="email"
+                name="email"
+              />
+            </label>
+          </div>
+          <div
+            className="password"
+          >
+            <label
+              htmlFor="password"
+            >
+              Password
+              <input
+                data-testid="password-input"
+                type="password"
+                id="password"
+                value=""
+                placeholder="password"
+                name="password"
+              />
+            </label>
+          </div>
+          <div
+            className="button"
+          >
+            <button type="button">Entrar</button>
+          </div>
+        </form>
+      </main>
+    );
   }
 }
 
