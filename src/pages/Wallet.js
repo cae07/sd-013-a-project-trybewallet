71ffa2da4f320bdca39f9630abbe80ab6ee056ac
Victor Diniz <victoralbertocostadiniz@gmail.com>
2021-08-30 16:38:23 -0300

    Completa o quarto e quinto requisitos, enquanto espero na mentoria para ajuda no segundo e terceiro que funcionam e não passam no avaliador

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      totalExpenses: 0,
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail } = this.props;
+    const { totalExpenses } = this.state;
+    return (
+      <div>
+        <header>
+          <h4 data-testid="email-field">{userEmail}</h4>
+          <h4 data-testid="total-field">{totalExpenses}</h4>
+          <h4 data-testid="header-currency-field">BRL</h4>
+        </header>
+      </div>);
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
