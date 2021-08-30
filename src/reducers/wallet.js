d6eb2d9fa7444091e72beb7880694364b7ef456f
Cássio Pereira <cassiorodp@gmail.com>
2021-08-30 19:26:49 -0300

    requisito 8 completo

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,4 +1,4 @@
-import { COINS_LOADING, COINS_SUCCESS, SPENT_SUCCESS } from '../actions';
+import { COINS_LOADING, COINS_SUCCESS, COINS_FAIL, SPENT_SUCCESS } from '../actions';
 
 const INICIAL_STATE = {
   currencies: [],
@@ -10,18 +10,25 @@ const wallet = (state = INICIAL_STATE, action) => {
   case COINS_LOADING:
     return {
       ...state,
-      coinsLoading: true,
+      isLoading: true,
     };
   case COINS_SUCCESS:
     return {
       ...state,
-      coinsLoading: false,
+      isLoading: false,
       currencies: action.coins,
+    };
+  case COINS_FAIL:
+    return {
+      ...state,
+      isLoading: false,
+      error: action.error,
     };
   case SPENT_SUCCESS:
     return {
       ...state,
       expenses: [...state.expenses, { ...action.spent, id: state.expenses.length }],
+      isLoading: false,
     };
   default:
     return state;
