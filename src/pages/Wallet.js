954f15c57cd3167d9f36076b64c1bf548a8300bf
Victor Diniz <victoralbertocostadiniz@gmail.com>
2021-08-31 18:35:17 -0300

    Completa o requisito 9

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -42,8 +42,8 @@ class Wallet extends React.Component {
       currency: 'USD',
       value: 0,
       description: '',
-      method: 'cash',
-      tag: 'food',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
     });
     const askForExpenseCurrency = await fetch('https://economia.awesomeapi.com.br/json/all')
       .then((response) => response.json())
@@ -72,6 +72,48 @@ class Wallet extends React.Component {
     });
   }
 
+  renderExpenseTable() {
+    const { expenses } = this.props;
+    return expenses.map((expense) => {
+      const { value, description, method, tag, currency, exchangeRates } = expense;
+      const askPrice = exchangeRates[currency].ask;
+      const expenseInBrl = Math.round((value * askPrice) * 100) / 100;
+      const usedCurrency = exchangeRates[currency];
+      const roundAsk = Math.round((usedCurrency.ask) * 100) / 100;
+      return (
+        <tr key={ expense.id } id={ expense.id }>
+          <td>
+            {description}
+          </td>
+          <td>
+            {tag}
+          </td>
+          <td>
+            {method}
+          </td>
+          <td>
+            {value}
+          </td>
+          <td>
+            {usedCurrency.name}
+          </td>
+          <td>
+            {roundAsk}
+          </td>
+          <td>
+            {expenseInBrl}
+          </td>
+          <td>
+            Real
+          </td>
+          <td>
+            <button type="button">Deletar</button>
+          </td>
+        </tr>
+      );
+    });
+  }
+
   render() {
     const { userEmail } = this.props;
     const { totalExpenses, value, description, method, tag, currency } = this.state;
@@ -83,7 +125,6 @@ class Wallet extends React.Component {
           <h4 data-testid="header-currency-field">BRL</h4>
         </header>
         <WalletBody
-          totalExpenses={ totalExpenses }
           value={ value }
           d={ description }
           method={ method }
@@ -92,6 +133,20 @@ class Wallet extends React.Component {
           handleChange={ this.handleChange }
           saveExpense={ this.saveExpense }
         />
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+          {this.renderExpenseTable()}
+        </table>
       </div>);
   }
 }
@@ -100,10 +155,12 @@ Wallet.propTypes = {
   userEmail: PropTypes.string.isRequired,
   savePairs: PropTypes.func.isRequired,
   expenseSaving: PropTypes.func.isRequired,
+  expenses: PropTypes.objectOf().isRequired,
 };
 
 const mapStateToProps = (state) => ({
   userEmail: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 const mapDispatchToProps = (dispatch) => ({
