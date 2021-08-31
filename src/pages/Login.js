9716a859c8f80944f0b94126676dfa9609170347
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-08-30 21:32:06 -0300

    Adding requirement 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,46 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState((state) => ({
+      ...state,
+      [target.name]: target.value,
+    }));
+  }
+
   render() {
+    const { email, password } = this.state;
+    const REG_EX_EMAIL = /^([\w\d._\-#])+@([\w\d._\-#]+[.][\w\d._\-#]+)+$/;
+    const minPassLength = 6;
     return (
       <div>
         <form>
           <input
             data-testid="email-input"
             type="email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
           />
           <input
             data-testid="password-input"
             type="password"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
           />
           <button
             type="submit"
+            disabled={ !REG_EX_EMAIL.test(email) || password.length < minPassLength }
           >
             Entrar
           </button>
