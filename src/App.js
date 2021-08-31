1652e63d43c62becd80ef0c136081d5a03dacda7
Gabriel Priss <gabrielpriss@hotmail.com>
2021-08-31 17:34:07 -0300

    iniciando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Wallet from './pages/Wallet';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+      </Switch>
+    </Router>);
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,38 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    function validation() {
+      const form = document.getElementById('form');
+      const emailInput = document.getElementById('email-input').value;
+      const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+      if (emailInput.match(emailPattern))
+      {
+        form.classList.add('valid');
+        form.classList.remove('invalid');
+      }
+      else
+      {
+        form.classList.add('invalid');
+        form.classList.remove('valid');
+      }
+    }
+    return (
+      <div>
+        <form id="form">
+          Email
+          <input
+            data-testid="email-input"
+            id="email-input"
+            placeholder="Coloque seu Email"
+            onKeyDown="validation()"
+          />
+          Senha
+          <input data testid="password-input" />
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
