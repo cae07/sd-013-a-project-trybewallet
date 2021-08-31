7dfacf41e20a44d56d2a29ef0f99c79535a4687a
Leonardo Santos <lcds90@gmail.com>
2021-08-31 13:42:14 -0300

    req 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,6 +6,7 @@ class Wallet extends React.Component {
   constructor(props) {
     super(props);
     this.renderHeader = this.renderHeader.bind(this);
+    this.renderForm = this.renderForm.bind(this);
   }
 
   renderHeader() {
@@ -19,10 +20,54 @@ class Wallet extends React.Component {
     );
   }
 
+  renderForm() {
+    return (
+      <article>
+        <form>
+          <label htmlFor="value">
+            Valor
+            <input type="text" id="value" />
+          </label>
+          <label htmlFor="description">
+            Descrição
+            <input type="text" id="description" />
+          </label>
+          <label htmlFor="currency">
+            Moeda
+            <select type="text" id="currency">
+              <option value="">Nada</option>
+            </select>
+          </label>
+          <label htmlFor="payment">
+            Método de pagamento
+            <select type="text" id="payment">
+              <option value="money">Dinheiro</option>
+              <option value="credit">Cartão de crédito</option>
+              <option value="debit">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="despesa">
+            Tag
+            <select type="text" id="despesa">
+              <option value="food">Alimentação</option>
+              <option value="house">Lazer</option>
+              <option value="work">Trabalho</option>
+              <option value="traffic">Transporte</option>
+              <option value="health">Saúde</option>
+            </select>
+          </label>
+        </form>
+      </article>
+    );
+  }
+
   render() {
     return (
       <main>
         {this.renderHeader()}
+        <section>
+          {this.renderForm()}
+        </section>
       </main>
     );
   }
