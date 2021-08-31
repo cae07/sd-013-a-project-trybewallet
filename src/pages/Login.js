54abb288761ffd90e50a9c6ab9422517c1bfae3e
JeanVPacheco <jeanvitorp@gmail.com>
2021-08-31 14:18:52 -0300

    Primeiro requisito, pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,48 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ value, name }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { login, password } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <input
+          type="text"
+          name="login"
+          value={ login }
+          placeholder="E-mail"
+          data-testid="email-input"
+        />
+        <h1>Password</h1>
+        <input
+          type="text"
+          name="password"
+          value={ password }
+          placeholder="Senha"
+          data-testid="password-input"
+        />
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
