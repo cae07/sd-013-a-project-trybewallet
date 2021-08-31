bd70e5cf9366aefbc64a61281a5389ba44d45f81
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-08-30 22:14:07 -0300

    resolvendo erros do lint

--- src/components/LoginInput.jsx
+++ src/components/LoginInput.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class LoginInput extends Component {
   constructor() {
@@ -7,23 +8,27 @@ class LoginInput extends Component {
   }
 
   render() {
-    const {func, login} = this.props;
+    const { func, login } = this.props;
 
     return (
       <label htmlFor="login">
         Login
         <input
-         type="text"
-         id="login"
-         name="email"
-         value={ login }
-         data-testid="email-input"
-         onChange={func}
-          />
+          type="text"
+          id="login"
+          name="email"
+          value={ login }
+          data-testid="email-input"
+          onChange={ func }
+        />
       </label>
-    )
+    );
   }
 }
 
+LoginInput.propTypes = {
+  func: PropTypes.func,
+  login: PropTypes.string,
+}.isRequired;
 
 export default LoginInput;
--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class PasswordInput extends Component {
-  constructor(props) {
-    super(props);
-
-  }
 
   render() {
-    const {func, pass} = this.props;
+    const { func, pass } = this.props;
 
     return (
       <label htmlFor="password">
         Password
         <input
-        type="password"
-        id="password"
-        name="password"
-        value={pass}
-        data-testid="password-input"
-        onChange={func}
+          type="password"
+          id="password"
+          name="password"
+          value={ pass }
+          data-testid="password-input"
+          onChange={ func }
         />
       </label>
-    )
+    );
   }
 }
+
+PasswordInput.propTypes = {
+  func: PropTypes.func,
+  pass: PropTypes.string,
+}.isRequired;
 
 export default PasswordInput;
