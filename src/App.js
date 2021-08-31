fcc500b1e00f7b791cca91091ce3791b7a00c73e
Josue Lobo <darksiders20005@gmail.com>
2021-08-31 19:11:30 -0300

    route wallet adicionada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
       </Switch>
     </div>
   );
