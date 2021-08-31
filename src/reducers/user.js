212ffff9d5366c1bff2d86ac293d6a4267be2b38
Isabella Antunes <bella.antunes2@gmail.com>
2021-08-31 15:24:50 -0300

    Configurando a store e reducers para iniciar o projeto

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,19 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import {} from '../actions';
+
+const INITIAL_STATE = {
+
+};
+
+const personalInformation = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case XXXXXX:
+    return {
+      ...state,
+    };
+  default:
+    return state;
+  }
+};
+
+export default personalInformation;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,19 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import {} from '../actions';
+
+const INITIAL_STATE = {
+
+};
+
+const walletInformation = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case XXXXXX:
+    return {
+      ...state,
+    };
+  default:
+    return state;
+  }
+};
+
+export default walletInformation;
