8af48cc2586f613ac0826d5f5c9d2084c91a39af
Thiago <thiagoc.carboneri@gmail.com>
2021-08-31 14:26:31 -0300

    req 1 ao 5

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import setLogin from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
+
+  onSubmitForm() {
+    const { email, password } = this.state;
+    const { history, submitLogin } = this.props;
+    submitLogin(email, password);
+    history.push('/carteira');
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
   render() {
+    const MAX = 6;
+    const { email, password } = this.state;
+    const validate = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email);
     return (
       <main>
         <form>
-          <div
-            className="email"
-          >
-            <label
-              htmlFor="email"
-            >
+          <div className="email">
+            <label htmlFor="email">
               Email
               <input
                 data-testid="email-input"
                 type="text"
                 id="email"
-                value=""
-                placeholder="email"
+                value={ email }
+                onChange={ (e) => this.handleChange(e) }
                 name="email"
               />
             </label>
@@ -25,24 +51,26 @@ class Login extends React.Component {
           <div
             className="password"
           >
-            <label
-              htmlFor="password"
-            >
+            <label htmlFor="password">
               Password
               <input
                 data-testid="password-input"
                 type="password"
                 id="password"
-                value=""
-                placeholder="password"
+                value={ password }
+                onChange={ (e) => this.handleChange(e) }
                 name="password"
               />
             </label>
           </div>
-          <div
-            className="button"
-          >
-            <button type="button">Entrar</button>
+          <div className="button">
+            <button
+              disabled={ !validate || password.length < MAX }
+              type="button"
+              onClick={ this.onSubmitForm }
+            >
+              Entrar
+            </button>
           </div>
         </form>
       </main>
@@ -50,4 +78,15 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  submitLogin: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  submitLogin: (...payload) => dispatch(setLogin(...payload)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
