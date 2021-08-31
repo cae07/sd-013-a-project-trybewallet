bf9ed8430d45e46b66ff21981c02769d68cad3da
luiz furtado <luizfurtadomkt@hotmail.com>
2021-08-30 21:06:29 -0300

    resolvendo erro de import

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
-import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <BrowserRouter>
