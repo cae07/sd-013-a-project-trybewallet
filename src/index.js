a226bc65c4c7dd329820585552cfaf7a33538ba1
Yuri <yurifranco18@live.com>
2021-08-31 15:34:43 -0300

    foi feito req. 1 a 6

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+
   document.getElementById('root'),
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,79 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { saveEmailAction } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      redirect: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState((state) => ({
+      ...state,
+      [target.name]: target.value,
+    }));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { saveEmail } = this.props;
+    const { email } = this.state;
+    const { history } = this.props;
+    console.log(email);
+    saveEmail(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, redirect } = this.state;
+    const REG_EX_EMAIL = /^([\w\d._\-#])+@([\w\d._\-#]+[.][\w\d._\-#]+)+$/;
+    const minPassLength = 6;
+    if (redirect) { return <Redirect to="/carteira" />; }
+    return (
+      <div>
+        <form onSubmit={ this.handleSubmit }>
+          <input
+            data-testid="email-input"
+            type="text"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <input
+            data-testid="password-input"
+            type="text"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="submit"
+            disabled={ !REG_EX_EMAIL.test(email) || password.length < minPassLength }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  saveEmail: (email) => dispatch(saveEmailAction(email)),
+});
+
+Login.propTypes = {
+  saveEmail: PropTypes.func.isRequired,
+  history: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
