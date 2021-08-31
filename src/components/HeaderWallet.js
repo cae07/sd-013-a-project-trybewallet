2e2f6a2e88ad67ff45621a54829fd3573e2b92ae
PC <sthecso>
2021-08-31 18:37:13 -0300

    Requisito 5

--- src/components/HeaderWallet.js
+++ src/components/HeaderWallet.js
@@ -3,13 +3,29 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      despesaTotal: 0,
+    };
+  }
+
   render() {
     const { stateEmail } = this.props;
+    const { despesaTotal } = this.state;
     return (
       <div>
         <header>
-          <div data-testid="email-field" />
-          {stateEmail}
+          <div data-testid="email-field">
+            {stateEmail}
+          </div>
+          <div data-testid="total-field">
+            Despesa Total R$
+            {despesaTotal}
+          </div>
+          <div data-testid="header-currency-field">
+            BRL
+          </div>
         </header>
       </div>
     );
