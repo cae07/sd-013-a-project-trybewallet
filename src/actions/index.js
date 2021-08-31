b9f47f44b72124bc8ec88b83d16f1c74d4da604c
Josue Lobo <darksiders20005@gmail.com>
2021-08-31 19:22:52 -0300

    actions atualizadas

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 export const LOGIN_NAME = 'LOGIN_NAME';
+export const WALLET = 'WALLET';
 
 export const loginName = (payload) => ({
   type: LOGIN_NAME,
+  payload,
+});
+
+export const wallet = (payload) => ({
+  type: WALLET,
   payload,
 });
