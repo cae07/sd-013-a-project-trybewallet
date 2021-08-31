763e6c74c2435d232a658245088611bdaa5af61a
Josue Lobo <darksiders20005@gmail.com>
2021-08-31 19:45:18 -0300

    funcões de validações implementadas

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.loginValidation = this.loginValidation.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    handleEmail(email);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value }, () => {
+      this.loginValidation();
+    });
+  }
+
+  loginValidation() {
+    const { email, password } = this.state;
+    let disabled = false;
+    const EMAIL_VALIDATION = /^[\w]+@([\w]+\.)+[\w]{2,4}$/gi;
+    const MIN_PASSWORD_LENGTH = 6;
+    disabled = !(EMAIL_VALIDATION.test(email) && password.length >= MIN_PASSWORD_LENGTH);
+    this.setState({ disabled });
+  }
+
   render() {
+    const { email, password, disabled } = this.state;
     return (
-      <div>
-        <input data-testid="email-input" />
-        <input data-testid="password-input" />
-        <button type="button">Entrar</button>
-      </div>
+      <main>
+        <form>
+          <input
+            type="text"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+            data-testid="password-input"
+          />
+          <button
+            type="submit"
+            disabled={ disabled }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </form>
+      </main>
     );
   }
 }
