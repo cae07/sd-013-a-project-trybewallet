844336b17b6259acac7daa0d2ce936408e8c8759
Seu nomecd <canaldogildosantos@gmail.com>
2021-08-31 16:19:41 -0300

    desafio 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,24 +3,26 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     return (
-      <fieldset>
-        <label htmlFor="email">
-          Email:
-          <input type="E-mail" name="email" data-tesId="email-input" onChange={ 0 } />
-        </label>
-        <label htmlFor="password">
-          Senha:
-          <input
-            type="text"
-            name="password"
-            data-testId="password-input"
-            onChange={ 0 }
-          />
-        </label>
-        <button type="button">
-          Entrar
-        </button>
-      </fieldset>
+      <div>
+        <fieldset>
+          <label htmlFor="email">
+            Email:
+            <input type="E-mail" name="email" data-testid="email-input" onChange={ 0 } />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input
+              type="text"
+              name="password"
+              data-testid="password-input"
+              onChange={ 0 }
+            />
+          </label>
+          <button type="submit">
+            Entrar
+          </button>
+        </fieldset>
+      </div>
     );
   }
 }
