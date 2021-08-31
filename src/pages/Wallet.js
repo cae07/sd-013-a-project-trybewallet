8f8463c45d2d779311bb07d2e3721a087631502d
andzoni <lorenzoniandre@outlook.com>
2021-08-31 21:13:46 +0100

    Refactor Requirement 06

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,44 +1,74 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import Form from '../components/Form';
 
 class Wallet extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.renderHeader = this.renderHeader.bind(this);
+    this.renderForm = this.renderForm.bind(this);
+  }
+
+  renderHeader() {
     const { emailInput } = this.props;
     return (
-      <div>
-        <header>
-          <h2 data-testid="email-field">
-            {emailInput}
-          </h2>
-          <h2 data-testid="total-field">
-            {0}
-          </h2>
-          <h2 data-testid="header-currency-field">
-            BRL
-          </h2>
-        </header>
+      <header>
+        <section data-testid="email-field">{emailInput}</section>
+        <section data-testid="total-field">0</section>
+        <section data-testid="header-currency-field">BRL</section>
+      </header>
+    );
+  }
+
+  renderForm() {
+    return (
+      <article>
         <form>
-          <label htmlFor="valor">
+          <label htmlFor="value">
             Valor
-            <input
-              id="valor"
-              type="text"
-              placeholder="Valor"
-            />
+            <input type="text" id="value" />
+          </label>
+          <label htmlFor="description">
+            Descrição
+            <input type="text" id="description" />
+          </label>
+          <label htmlFor="currency">
+            Moeda
+            <select type="text" id="currency">
+              <option value="">Nada</option>
+            </select>
+          </label>
+          <label htmlFor="payment">
+            Método de pagamento
+            <select type="text" id="payment">
+              <option value="money">Dinheiro</option>
+              <option value="credit">Cartão de crédito</option>
+              <option value="debit">Cartão de débito</option>
+            </select>
           </label>
           <label htmlFor="despesa">
-            Descrição
-            <input
-              id="despesa"
-              type="text"
-              placeholder="Despesa"
-            />
+            Tag
+            <select type="text" id="despesa">
+              <option value="food">Alimentação</option>
+              <option value="house">Lazer</option>
+              <option value="work">Trabalho</option>
+              <option value="traffic">Transporte</option>
+              <option value="health">Saúde</option>
+            </select>
           </label>
-          <Form />
         </form>
-      </div>
+      </article>
+    );
+  }
+
+  render() {
+    return (
+      <main>
+        {this.renderHeader()}
+        <section>
+          {this.renderForm()}
+        </section>
+      </main>
     );
   }
 }
