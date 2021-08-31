c112e0782aa9780ca3dc9007ec55a33f70577202
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-08-31 18:43:54 -0300

    lint

--- src/components/FormAddExpenditure.js
+++ src/components/FormAddExpenditure.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 
-import { Alert } from 'bootstrap';
 import { Input, Select } from './index';
 import { fetchCurrencies, addExpense, totalUpdate } from '../actions';
 
