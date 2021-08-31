0791a970d88fee348bdf0ca2db4777bd3a248910
deyvid-dev <deyvidscm@hotmail.com>
2021-08-31 19:02:06 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input htmlFor="" data-testid="email-input" />
+        Email
+        <input htmlFor="" data-testid="password-input" />
+        Senha
+        <button type="submit">Entrar</button>
+      </form>
+    );
   }
 }
 
