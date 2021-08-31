f292b48f03295ab1fd47502d69c3e65983b5cca1
luiz furtado <luizfurtadomkt@hotmail.com>
2021-08-30 21:02:33 -0300

    teste eslint

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={store}>
+    <Providerider store={ store }>
       <App />
     </Provider>
   </BrowserRouter>,
