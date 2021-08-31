9d27ff18be9d6a447b8fae38c12b6eb8875686c8
Luiza Antiques <luiza.antiques@gmail.com>
2021-08-31 01:27:25 -0300

    Conclui requisito 7

--- src/components/FormWallet.js
+++ src/components/FormWallet.js
@@ -2,9 +2,44 @@ import React, { Component } from 'react';
 import { Inputs, Select } from '.';
 
 class FormWallet extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currencyOptions: [],
+      // value: '',
+      // description: '',
+      // currency: '',
+      // payment: '',
+      // tag: '',
+    };
+
+    this.fetchAPI = this.fetchAPI.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  async fetchAPI() {
+    const obj = await (await fetch('https://economia.awesomeapi.com.br/json/all')).json();
+    const result = Object.keys(obj).filter((item) => item !== 'USDT');
+    this.setState({
+      currencyOptions: result,
+    });
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     const payment = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
     const tag = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
+    const { currencyOptions } = this.state;
 
     return (
       <form>
@@ -24,11 +59,11 @@ class FormWallet extends Component {
             onHandleChange={ this.handleChange }
           />
           <Select
-            name="coin"
+            name="currency"
             page="wallet"
             label="Moeda"
             onHandleChange={ this.handleChange }
-            options={ [] }
+            options={ currencyOptions }
           />
           <Select
             name="payment"
