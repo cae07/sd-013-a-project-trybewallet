89b308d07f0e489521241338bd9e471d3627fa50
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-08-30 21:35:26 -0300

    Configurando Store, Actions e Reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,14 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'show':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default user;
