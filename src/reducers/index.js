5b374df8bb5c91e47f01df7098f27354e4556e82
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-08-31 15:42:01 -0300

    update .css

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,12 +8,12 @@ import thunk from 'redux-thunk';
 import user from './user';
 // import wallet from './wallet';
 
-// Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
-
 export const rootReducer = combineReducers({ user });
 
 export const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk)),
 );
+
+// Configure os seus reducers.
+// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
