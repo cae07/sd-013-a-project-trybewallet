8533af92720cba98209d7a0ea949e124985a4455
Isabella Antunes <bella.antunes2@gmail.com>
2021-08-31 18:25:06 -0300

    Mais uma tentativa de correção do lint

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from './store/index';
 import './index.css';
+import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
