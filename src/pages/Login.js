d371674b2f6d03c9eff7233a2a5090045f1c6887
Josue Lobo <darksiders20005@gmail.com>
2021-09-01 18:47:17 +0000

    lint 2.0

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -41,7 +41,7 @@ class Login extends Component {
         },
       });
     }
-    this.state({ validate: { ...validate, login: false } })
+    this.state({ validate: { ...validate, login: false } });
   }
 
   passwordValidation() {
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -2,12 +2,12 @@ const INITIAL_STATE = {
   currencies: [],
   expenses: [],
 };
-  
+
 const walletReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   default:
     return state;
   }
 };
-  
+
 export default walletReducer;
