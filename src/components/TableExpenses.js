30536ec69eafcc50a799f82e41d0891864855d0e
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-09-01 16:21:46 -0300

    Requisito 9 feito

--- src/components/TableExpenses.js
+++ src/components/TableExpenses.js
@@ -1,24 +1,71 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
 
 class TableExpenses extends React.Component {
+  test() {
+    const { listExpenses } = this.props;
+
+    return listExpenses
+      .map(({ description, tag, method, value, currency, exchangeRates, id }) => {
+        const valNumber = parseFloat(value);
+        const atualValue = parseFloat(exchangeRates[currency].ask);
+        const currencyConvert = (atualValue * valNumber).toFixed(2);
+        return (
+          <tr key={ id }>
+            <td>{id + 1}</td>
+            <td>{description}</td>
+            <td>{tag}</td>
+            <td>{method}</td>
+            <td>{valNumber}</td>
+            <td>{exchangeRates[currency].name.split('/')[0]}</td>
+            <td>{atualValue.toFixed(2)}</td>
+            <td>{currencyConvert}</td>
+            <td>Real</td>
+          </tr>
+        );
+      });
+  }
+
   render() {
     return (
-      <div>
-        <table>
-          <thead>
+      <div className="col-md-10 mx-auto">
+        <table className="table table-hover">
+          <thead className="text-white bg-dark text-center">
             <tr>
-              <th>wqewq</th>
-              <th>wqewq</th>
-              <th>wqewq</th>
-              <th>wqewq</th>
-              <th>wqewq</th>
-              <th>wqewq</th>
+              <th scope="col">#</th>
+              <th scope="col">Descrição</th>
+              <th scope="col">Tag</th>
+              <th scope="col">Método de pagamento</th>
+              <th scope="col">Valor</th>
+              <th scope="col">Moeda</th>
+              <th scope="col">Câmbio utilizado</th>
+              <th scope="col">Valor convertido</th>
+              <th scope="col">Moeda de conversão</th>
+              <th scope="col">Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {this.test()}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default TableExpenses;
+const mapStateToProps = ({ wallet }) => ({ listExpenses: wallet.expenses });
+
+TableExpenses.propTypes = {
+  listExpenses: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    value: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+    method: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    exchangeRates: PropTypes.objectOf(PropTypes.string).isRequired,
+  })).isRequired,
+};
+
+export default connect(mapStateToProps)(TableExpenses);
