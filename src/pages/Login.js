e3e25efb73578bb66bdd35b25e68519cab39f2ea
ivandosss <ivanndosssilva9389@gmail.com>
2021-09-01 00:34:13 -0300

    Primeiro Requisito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { setLogin } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleClick() {
+    const { history, dispatchSetValue } = this.props;
+    dispatchSetValue(this.state);
+    history.push('/');
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <form>
+          <label htmlFor="email-input">
+            <input type="text" data-testid="email-input" />
+          </label>
+          <label htmlFor="password-input">
+            <input type="password" data-testid="password-input" />
+          </label>
+          <button type="submit" onClick={ this.handleClick }>Entrar</button>
+        </form>
+      </section>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatchSetValue: PropTypes.func,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
+
+const mapStateToProps = (state) => ({ inputLogin: state.user.inputLogin });
+const mapDispatchToProps = (dispath) => ({
+  dispathSetValue: (stateLogins) => dispath(setLogin(stateLogins)) });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SET_LOGIN } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case SET_LOGIN:
+    return { ...state, inputLogin: action.payload };
+  default:
+    return state;
+  }
+};
+
+export default reducer;
