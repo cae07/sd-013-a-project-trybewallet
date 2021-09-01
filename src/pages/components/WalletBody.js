012216f7d86af71e5fe088aec15af2bb32de1192
Victor Diniz <victoralbertocostadiniz@gmail.com>
2021-09-01 16:58:29 -0300

    Tenta arrumar css e quebra o programa XD

--- src/pages/components/WalletBody.js
+++ src/pages/components/WalletBody.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { InputGroup } from 'react-bootstrap'
 
 class WalletBody extends Component {
   constructor() {
