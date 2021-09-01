95dddc596d6ecc4b0dbd5816307e6378137e3a81
Joao Santos <jvitor.spaula@gmail.com>
2021-09-01 20:29:41 -0300

    Requisito 6.1

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Form from '../components/form';
+import Form from '../components/Form';
 
 class Wallet extends React.Component {
   render() {
