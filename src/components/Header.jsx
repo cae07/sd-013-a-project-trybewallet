65d65041af0a144079a09898e45f52a10d1b5d6b
Wesley Maia <maia.weol@gmail.com>
2021-09-01 19:17:40 -0300

    Requisito: 5- Crie um header para a página <Header />

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends React.Component {
   render() {
+    const { email } = this.props;
+    const total = 0;
     return (
       <header>
-        Ok
+        <span data-testid="email-field">
+          {email}
+        </span>
+        <span data-testid="total-field">
+          {`Despesa Total: R$ ${total}`}
+        </span>
+        <span data-testid="header-currency-field">
+          BRL
+        </span>
       </header>
     );
   }
 }
 
-const mapStateToProps = () => ({ });
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
 
 export default connect(mapStateToProps, null)(Header);
