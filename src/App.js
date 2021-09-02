e4ad634e771c11c96062704768d73f302a706b17
Felipe Dias <felipeferreiradias1@gmail.com>
2021-09-01 22:07:00 -0300

    add req 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import './App.css';
 
 function App() {
-  return <div>Hello,  TrybeWallet!</div>;
+  return (
+    <div>
+      {' '}
+      Hello, TrybeWallet!
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/carteira" component={ Wallet } />
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,84 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      buttonDisabled: true,
+    };
+    this.handleButton = this.handleButton.bind(this);
+    this.validLogin = this.validLogin.bind(this);
+  }
+
+  // função para validar email usando regex
+  validEmail(email) {
+    const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    return reg.test(email);
+  }
+
+  // função para validar tamanho da senha
+  validPassword(password) {
+    const MIN_LENGHT = 6;
+    return password.length >= MIN_LENGHT;
+  }
+
+  // Essa função ira validar a senha e o email da tela de login
+  validLogin() {
+    const { email, password } = this.state;
+    if (this.validEmail(email) && this.validPassword(password)) {
+      this.setState({
+        buttonDisabled: false,
+      });
+    } else {
+      this.setState({
+        buttonDisabled: true,
+      });
+    }
+  }
+
+  // handleButton genérico para qualquer input do formulário
+  handleButton({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value }, () => {
+      this.validLogin();
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { buttonDisabled } = this.state;
+    return (
+      <div>
+        <input
+          type="email"
+          placeholder="Insira seu email"
+          name="email"
+          /* value="" */
+          onChange={ this.handleButton }
+          data-testid="email-input"
+        />
+        <input
+          type="text"
+          placeholder="Insira sua senha"
+          name="password"
+          /* value="" */
+          onChange={ this.handleButton }
+          data-testid="password-input"
+        />
+
+        <button
+          /* onClick={ () => setCart({ id, title, price, quant: 1, thumbnail, available }) } */
+          type="button"
+          data-testid="product-add-to-cart"
+          disabled={ buttonDisabled }
+        >
+          entrar
+        </button>
+      </div>
+    );
   }
 }
 
