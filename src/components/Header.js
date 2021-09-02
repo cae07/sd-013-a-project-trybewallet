3e9a4d5e43d492ab8008bec3b10c1b79c425c126
marcos <marc0sdurval20@gmail.com>
2021-09-02 11:59:06 -0300

    req8

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,39 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      valor: [],
+      soma: 0,
+    };
+    this.auxiliar = this.auxiliar.bind(this);
+  }
+
   componentDidUpdate() {
-    const { props } = this;
-    console.log(props);
+    this.auxiliar();
+  }
+
+  auxiliar() {
+    const { props: { dados }, state: { valor } } = this;
+    if (dados !== valor) {
+      let final = 0;
+      dados.forEach((element) => {
+        const value = parseFloat(element.value);
+        const name = element.currency;
+        Object.keys(element.exchangeRates).forEach((moeda) => {
+          if (name === moeda) {
+            const cotacao = parseFloat(element.exchangeRates[moeda].ask);
+            final = Math.round((cotacao * value) * 100) / 100;
+          }
+        });
+      });
+      this.setState((prevent) => ({ valor: dados, soma: prevent.soma + final }));
+    }
   }
 
   render() {
-    console.log(this.props);
-    const { emailUser } = this.props;
+    const { props: { emailUser }, state: { soma } } = this;
     return (
       <div>
         <h6 data-testid="email-field">
@@ -19,7 +44,7 @@ class Header extends React.Component {
         <h6>
           Depesas totais:
         </h6>
-        <p data-testid="total-field">0</p>
+        <p data-testid="total-field">{soma}</p>
         <p data-testid="header-currency-field">BRL</p>
       </div>
     );
@@ -27,10 +52,12 @@ class Header extends React.Component {
 }
 Header.propTypes = {
   emailUser: PropTypes.string.isRequired,
+  dados: PropTypes.arrayOf([]).isRequired,
 };
+
 const mapStateToProps = (state) => ({
   emailUser: state.user.email,
-  teste: state.wallet.expenses,
+  dados: state.wallet.expenses,
 });
 
 export default connect(mapStateToProps)(Header);
