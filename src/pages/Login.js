15bd117277404577e7c0d9a808d8bc6331c75859
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-09-02 00:14:24 -0300

    Atende requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,14 +8,54 @@ class Login extends React.Component {
     super();
     this.state = {
       email: '',
+      initialEmail: false,
       password: '',
+      initialPassword: false,
     };
+    this.activateButtonClick = this.activateButtonClick.bind(this);
+    this.emailValidator = this.emailValidator.bind(this);
+    this.passwordValidator = this.passwordValidator.bind(this);
+  }
+
+  activateButtonClick() {
+    const buttonInfo = document.querySelector('.btn');
+    const { initialEmail, initialPassword } = this.state;
+    if ((initialEmail) && (initialPassword)) {
+      buttonInfo.removeAttribute('disabled');
+    } else {
+      buttonInfo.setAttribute('disabled', '');
+    }
+  }
+
+  emailValidator({ currentTarget }) {
+    const emailTarget = currentTarget;
+    const emailRegexValidation = /^\w+(\[\+\.-\]?\w)*@\w+(\[\.-\]?\w+)*\.[a-z]+$/i;
+    const testInfo = emailRegexValidation.test(emailTarget.value);
+    if (testInfo) {
+      this.setState({
+        initialEmail: true,
+      });
+    }
+    this.activateButtonClick();
+  }
+
+  passwordValidator({ currentTarget }) {
+    const passwordTarget = currentTarget;
+    const passwordRegexValidation = /^[0-9]{5,}$/i;
+    const testInfo = passwordRegexValidation.test(passwordTarget.value);
+    if (testInfo) {
+      this.setState({
+        initialPassword: true,
+      });
+    }
+    this.activateButtonClick();
   }
 
   handleClick() {
     const { history, dispathSetValue } = this.props;
     dispathSetValue(this.state);
     history.push('/');
+    // console.log('ativo');
   }
 
   render() {
@@ -29,6 +69,7 @@ class Login extends React.Component {
               placeholder="Email"
               required
               autoComplete="off"
+              onChange={ this.emailValidator }
             />
           </label>
           <label htmlFor="password-input">
@@ -38,9 +79,19 @@ class Login extends React.Component {
               placeholder="Password"
               required
               autoComplete="off"
+              onChange={ this.passwordValidator }
             />
           </label>
-          <button type="submit" onClick={ this.handleClick }>Entrar</button>
+          <button
+            type="submit"
+            onClick={
+              this.handleClick
+            }
+            className="btn"
+            disabled
+          >
+            Entrar
+          </button>
         </form>
       </section>
     );
