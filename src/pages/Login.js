04c583df6ce347db5eed3c1aaca7d5fbdc3af2d2
ivandosss <ivanndosssilva9389@gmail.com>
2021-09-02 00:14:47 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,14 +9,54 @@ class Login extends React.Component {
 
     this.state = {
       email: '',
+      initialEmail: false,
       password: '',
+      initialPassword: false,
     };
+    this.disabledButton = this.disabledButton.bind(this);
+    this.emailValidation = this.emailValidation.bind(this);
+    this.validatorPassword = this.validatorPassword.bind(this);
+  }
+
+  disabledButton() {
+    const clickButton = document.querySelector('.btn');
+    const { initialEmail, initialPassword } = this.state;
+    if ((initialEmail) && (initialPassword)) {
+      clickButton.removeAttribute('disabled');
+    } else {
+      clickButton.setAttribute('disabled', '');
+    }
+  }
+
+  emailValidation({ currentTarget }) {
+    const email = currentTarget;
+    const regexEmail = /^\w+(\[\+\.-\]?\w)*@\w+(\[\.-\]?\w+)*\.[a-z]+$/i;
+    const testValue = regexEmail.test(email.value);
+    if (testValue) {
+      this.setState({
+        initialEmail: true,
+      });
+    }
+    this.disabledButton();
+  }
+
+  validatorPassword({ currentTarget }) {
+    const passwordValidator = currentTarget;
+    const regexPassword = /^[0-9]{5,}$/i;
+    const testValue = regexPassword.test(passwordValidator.value);
+    if (testValue) {
+      this.setState({
+        initialPassword: true,
+      });
+    }
+    this.disabledButton();
   }
 
   handleClick() {
-    const { history, dispatchSetValue } = this.props;
-    dispatchSetValue(this.state);
-    history.push('/');
+    // const { history, dispatchSetValue } = this.props;
+    // dispatchSetValue(this.state);
+    // history.push('/');
+    console.log('ativo');
   }
 
   render() {
@@ -24,12 +64,31 @@ class Login extends React.Component {
       <section>
         <form>
           <label htmlFor="email-input">
-            <input type="text" data-testid="email-input" />
+            <input
+              type="text"
+              required
+              placeholder="Email"
+              onChange={ this.emailValidation }
+              data-testid="email-input"
+            />
           </label>
           <label htmlFor="password-input">
-            <input type="password" data-testid="password-input" />
+            <input
+              type="password"
+              data-testid="password-input"
+              placeholder="Enter your password"
+              required
+              onChange={ this.validatorPassword }
+            />
           </label>
-          <button type="submit" onClick={ this.handleClick }>Entrar</button>
+          <button
+            className="btn"
+            type="submit"
+            onClick={ this.handleClick }
+            disabled
+          >
+            Entrar
+          </button>
         </form>
       </section>
     );
