a8f1ed80fd7263b1f55e01739d8f1bb4e9947a16
Matheus Duarte de Freitas <freickup@gmail.com>
2021-09-01 22:56:30 -0300

    criaçao e rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import NotFound from './pages/NotFound';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/wallet" component={ Wallet } />
+      <Route path="*" component={ NotFound } />
+    </Switch>
+  );
 }
 
 export default App;
