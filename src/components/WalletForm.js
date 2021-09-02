05f16497fdd8938baa3d4ec06faa34a336b19839
icaroMelo <exemployt@gmail.com>
2021-09-02 13:12:51 -0300

    Requisito 6

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -3,40 +3,48 @@ import React from 'react';
 class WalletForm extends React.Component {
   render() {
     return (
-      <form>
-        <label htmlFor="price">
-          Valor:
-          <input type="number" name="price" />
-        </label>
-        <label htmlFor="description">
-          Descrição:
-          <input type="text" name="description" />
-        </label>
-        <label htmlFor="currency">
-          Moeda
-          <select>
-            <option>BRL</option>
-          </select>
-        </label>
-        <label htmlFor="methodOfPayment">
-          Método de pagamento
-          <select>
-            <option>Dinheiro</option>
-            <option>Cartão de crédito</option>
-            <option>Cartão de débito</option>
-          </select>
-        </label>
-        <label htmlFor="tag">
-          Tag
-          <select>
-            <option>Alimentação</option>
-            <option>Lazer</option>
-            <option>Trabalho</option>
-            <option>Transporte</option>
-            <option>Saúde</option>
-          </select>
-        </label>
-      </form>
+      <div>
+        <form>
+          <label htmlFor="price">
+            Valor
+            <input type="number" name="price" id="price" />
+          </label>
+
+          <label htmlFor="description">
+            Descrição
+            <input type="text" name="description" id="description" />
+          </label>
+
+          <label htmlFor="currency">
+            Moeda
+            <select name="currency" id="currency">
+              <option>
+                Vazio
+              </option>
+            </select>
+          </label>
+
+          <label htmlFor="methodOfPayment">
+            Método de pagamento
+            <select name="methodOfPayment" id="methodOfPayment">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+
+          <label htmlFor="tag">
+            Tag
+            <select name="tag" id="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
+      </div>
     );
   }
 }
