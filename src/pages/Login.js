737daee8d4499d235e0f96b5f526d03956923098
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-09-02 18:08:21 -0300

    Req.1 + Inicio do 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      buttonDisable: true,
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { buttonDisable } = this.state;
+    return (
+      <div>
+        <input
+          data-testid="email-input"
+          placeholder="E-mail"
+        />
+        <input
+          data-testid="password-input"
+          placeholder="Password"
+        />
+        <button type="button" disabled={ buttonDisable }>
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
