99171d1da53b9324e247c98bf2d97af23884681f
Micael Santos <micaelmaicon@hotmail.com>
2021-09-03 09:03:58 -0300

    Store criada

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+// Criando store
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducer from '../reducers';
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
+
+export default store;
