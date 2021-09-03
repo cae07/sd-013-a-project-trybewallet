c8f79ab789fe48afb935698c390e90c08bb0ace4
Sethano <vitorcard1720@gmail.com>
2021-09-03 18:27:51 -0300

    requisito 7

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      moedas: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMoedas();
+  }
+
+  async fetchMoedas() {
+    const listaMoedas = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const data = await listaMoedas.json();
+    const moedas = Object.keys(data).filter((a) => a !== 'USDT');
+    this.setState({ moedas });
+  }
+
   render() {
+    const { moedas } = this.state;
     return (
       <form>
         <label htmlFor="valor">
@@ -17,7 +36,10 @@ class Form extends React.Component {
         <label htmlFor="moeda">
           Moeda
           <select id="moeda">
-            {}
+            { moedas.map((moeda) => (
+              <option value={ moeda } key={ moeda }>
+                { moeda }
+              </option>))}
           </select>
         </label>
         <label htmlFor="pagamento">
