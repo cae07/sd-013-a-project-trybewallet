faacdac1ff1e6d5b04c1fe19768fd1fe3895d086
Daniel <daniel.melo.oliveira27@gmail.com>
2021-09-02 23:11:32 -0300

    last fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
