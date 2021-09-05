56b97befdd2ae781641167badf91de9ebac406ac
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-09-04 21:15:13 -0300

    Desafio 1 feito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="email-input">
+          Email:
+          <input
+            type="text"
+            data-testid="email-input"
+          />
+        </label>
+        <label htmlFor="email-input">
+          Senha:
+          <input
+            type="number"
+            data-testid="password-input"
+          />
+        </label>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
