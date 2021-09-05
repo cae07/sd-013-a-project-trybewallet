8cced1cde6325ab3e694879f75197cacf90f26c9
sonkai <mfilho86@gmail.com>
2021-09-04 21:57:21 -0300

    troquei store.js por index pra não ficar redundante

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from './store/store';
+import store from './store/index';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
