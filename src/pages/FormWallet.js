120141a91671caf9cd1358ce83335b5c16b80e88
Seu nomecd <canaldogildosantos@gmail.com>
2021-09-05 18:47:56 -0300

    desafio6

--- src/pages/FormWallet.js
+++ src/pages/FormWallet.js
@@ -13,44 +13,44 @@ class FormWallet extends React.Component {
               name="valor"
               value=""
             />
-            </label>
-            <label htmlFor="descriçãoForm">
-              Descrição
-              <input
-                id="descriçãoForm"
-                type="text"
-                name="descrição"
-                value=""
-                />
-            </label>
-            <label htmlFor="moeda">
-              <select id="moeda">
-                Moeda
-                <option></option>
-              </select>
-            </label>
-            <label htmlFor="metodo"> 
-              Método de pagamento
-              <select id="metodo">
-                <option value="Dinheiro">Dinheiro</option>
-                <option value="Cartão de crádito">Cartão de crádito</option>
-                <option value="Cartão de débito">Cartão de débito</option>
-              </select>
-            </label>
-            <label htmlFor="tag">
-              Tag
-              <select id="tag">
-                <option>Alimentação</option>
-                <option>Lazer</option>
-                <option>Trabalho</option>
-                <option>Transporte</option>
-                <option>Saúde</option>
-              </select>
-            </label>
+          </label>
+          <label htmlFor="descriçãoForm">
+            Descrição
+            <input
+              id="descriçãoForm"
+              type="text"
+              name="descrição"
+              value=""
+            />
+          </label>
+          <label htmlFor="moeda">
+            <select id="moeda">
+              Moeda
+              <option>a</option>
+            </select>
+          </label>
+          <label htmlFor="metodo">
+            Método de pagamento
+            <select id="metodo">
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crádito">Cartão de crádito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
         </form>
-    </div>
-    )
+      </div>
+    );
+  }
 }
-}
-export default FormWallet;
 
+export default FormWallet;
