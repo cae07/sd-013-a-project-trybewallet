d37c5f6d1aced7c3f45734e1d244621a6a601431
Andrey Gois <gois.andrey@hotmail.com>
2021-09-06 11:34:41 -0300

    Add questão 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input
+          data-testid="email-input"
+          type="email"
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+        />
+        <button
+          type="submit"
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,8 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const userReducer = (state = {}, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
