bde57a95f42b9b8325c355154816d3754f83f8db
JOSE VICTOR SANTIAGO FIGUEIREDO BAPTISTA <78240695+jvictorbap@users.noreply.github.com>
2021-09-06 17:00:24 -0300

    requisito 1 ao 3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,3 @@
-// Coloque aqui suas actions
+export const GET_EMAIL = 'GET_EMAIL';
+
+export const getEmail = (payload) => ({ type: GET_EMAIL, email: payload });
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import './Login.css';
+
+import { getEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      valid: false,
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.changePage = this.changePage.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value }, () => {
+      const { email, password, valid } = this.state;
+      const regex = /(.)(.*)@(.)(.*)\.(...)(.*)/;
+      const passNum = 6;
+      if (email.match(regex) && password.length >= passNum && valid === false) {
+        this.setState({ valid: true });
+      }
+    });
+  }
+
+  changePage() {
+    const { history, dispatchEmail } = this.props;
+    const { email } = this.state;
+    dispatchEmail(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { valid } = this.state;
+    return (
+      <div className="login-panel">
+        <label htmlFor="email">
+          Email:
+          <input
+            name="email"
+            type="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            name="password"
+            type="text"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        {valid ? <button type="button" onClick={ this.changePage }>Entrar</button> : <button type="button" disabled>Entrar</button>}
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  dispatchEmail: (payload) => dispatch(getEmail(payload)),
+});
+
+Login.propTypes = {
+  history: PropTypes.object,
+}.isRequired;
+
+export default connect(null, mapDispatchToProps)(Login);
