f69340f19aa4caa235c1b7be2b0832f90fbf74d8
Wesley Maia <maia.weol@gmail.com>
2021-09-06 08:44:31 -0300

    Refatorando o código

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,6 @@
-import { LOGIN_ACTION, START_FETCH, GET_INFO, EXCHANGE_RATES } from './actionTypes';
 // Coloque aqui suas actions
 // Action é uma função que retorna um objeto que contenha ao menos uma chave denominada "type"
-
-// export const LOGIN_ACTION = 'LOGIN_ACTION';
+import { LOGIN_ACTION, START_FETCH, GET_INFO, EXCHANGE_RATES } from './actionTypes';
 
 export const loginAction = (email) => ({
   type: LOGIN_ACTION,
@@ -19,10 +17,7 @@ export const expenseList = (state) => ({ type: EXCHANGE_RATES, state });
 
 export const fetchCurrencies = () => async (dispatch) => {
   dispatch(startFetch());
-  return fetch('https://economia.awesomeapi.com.br/json/all')
-    .then((r) => r.json()
-      .then(
-        (json) => dispatch(getInfo(json)),
-        // (error) => dispatch(failedRequest(error)),
-      ));
+  const result = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const resultJson = await result.json();
+  dispatch(getInfo(resultJson));
 };
