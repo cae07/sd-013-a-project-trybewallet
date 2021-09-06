be652965a8dfc1f56c12fb757848b6caa6bb8fcd
Felipe Dias <felipeferreiradias1@gmail.com>
2021-09-06 19:35:54 -0300

    fix lint

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,46 +9,46 @@ class Form extends React.Component {
       <div>
         <label htmlFor="expense">
           Valor
+          <input
+            type="text"
+            id="expense"
+            name="expense"
+            /* onChange={ this.handleButton } */
+          />
         </label>
-        <input
-          type="text"
-          id="expense"
-          name="expense"
-          /* onChange={ this.handleButton } */
-        />
         <label htmlFor="description">
           Descrição
+          <input
+            type="text"
+            id="description"
+            name="description"
+            /* onChange={ this.handleButton } */
+          />
         </label>
-        <input
-          type="text"
-          id="description"
-          name="description"
-          /* onChange={ this.handleButton } */
-        />
         <label htmlFor="currency">
           Moeda
+          <select name="currency" id="currency">
+            <option value="valor1">Valor 1</option>
+          </select>
         </label>
-        <select name="currency" id="currency">
-          <option value="valor1">Valor 1</option>
-        </select>
         <label htmlFor="payment">
           Método de pagamento
+          <select name="payment" id="payment">
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
+          </select>
         </label>
-        <select name="payment" id="payment">
-          <option value="Dinheiro">Dinheiro</option>
-          <option value="Cartão de crédito">Cartão de crédito</option>
-          <option value="Cartão de débito">Cartão de débito</option>
-        </select>
         <label htmlFor="Tag">
           Tag
+          <select name="Tag" id="Tag">
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
         </label>
-        <select name="Tag" id="Tag">
-          <option value="Alimentação">Alimentação</option>
-          <option value="Lazer">Lazer</option>
-          <option value="Trabalho">Trabalho</option>
-          <option value="Transporte">Transporte</option>
-          <option value="Saúde">Saúde</option>
-        </select>
       </div>
     );
   }
