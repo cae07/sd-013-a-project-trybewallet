2eaffd4e365d66cce2746c4deb0f3b65a2e62d83
Felipe Dias <felipeferreiradias1@gmail.com>
2021-09-06 18:31:50 -0300

    req 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email, expenses = 0 } = this.props;
+    return (
+      <div>
+        <p data-testid="email-field">{ email }</p>
+        <p data-testid="total-field">
+          { `Despesa total: R$${expenses} ` }
+        </p>
+        <p data-testid="header-currency-field">BRL</p>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps, null)(Wallet);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,26 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// Cria um estado inicial para o reducer 'user'
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  isFetching: true,
+};
+
+// Cria o reducer user
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  // caso EMAIL_VALID, mantém o estado e altera a chave email.
+  case 'EMAIL_VALID':
+    return {
+      ...state,
+      email: action.currencies,
+    };
+
+    // caso não encontre nenhum caso, retorna o estado atual
+  default:
+    return state;
+  }
+};
+
+// exporta o reducer user que será utilizado ...
+export default wallet;
