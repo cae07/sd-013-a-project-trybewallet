d388f1d1eeb4d44765c317e47515b1f2ff738f6a
Luiz Casimiro <luizcasimi@gmail.com>
2021-09-07 11:38:24 -0300

    solve 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 class Wallet extends React.Component {
   render() {
     const { walletEmail } = this.props;
-
     return (
       <div>
         <header>
@@ -13,6 +12,42 @@ class Wallet extends React.Component {
           <h4 data-testid="total-field">{`Despesa Total: R$ ${0}`}</h4>
           <h4 data-testid="header-currency-field">BRL</h4>
         </header>
+        <form>
+          <label htmlFor="amount-input">
+            Valor:
+            <input id="amount-input" type="text" />
+          </label>
+          <label htmlFor="description-input">
+            Descrição:
+            <input id="description-input" type="text" />
+          </label>
+          <label htmlFor="currency-input">
+            Moeda:
+            <select id="currency-input">
+              <option>Moeda 01</option>
+              <option>Moeda 02</option>
+              <option>Moeda 03</option>
+            </select>
+          </label>
+          <label htmlFor="payment-input">
+            Método de Pagamento:
+            <select id="payment-input">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag-input">
+            Tag:
+            <select id="tag-input">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
