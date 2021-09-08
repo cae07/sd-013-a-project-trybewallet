ae32e44c105abd4a2525d51d3fdf7ec4211ee95e
Ramon Vitor <vitorramon@hotmail.com>
2021-09-07 22:20:54 -0300

    Requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -8,8 +8,8 @@ class Wallet extends React.Component {
     return (
       <header>
         <span data-testid="email-field">{ email }</span>
-        <span data-testid="total-field"> Dispensa total: 0 </span>
-        <span data-testid="header-currency-field"> câmbio </span>
+        <span data-testid="total-field">{ `Dispensa total: ${0}` }</span>
+        <span data-testid="header-currency-field">BRL</span>
       </header>
     );
   }
