1340d2b165284e360cce62e13406c6f0cdcb4de6
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-09-08 14:06:43 -0300

    COrrige

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import '../Header.css';
 
 class Header extends Component {
   constructor() {
