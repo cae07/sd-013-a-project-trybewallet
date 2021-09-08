fac243777992699cfdfdeeca98b36451934f7b46
Ramon Vitor <vitorramon@hotmail.com>
2021-09-08 10:29:14 -0300

    Adicionando os creditos as pessoas que me ajudaram no projeto

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
+// PROJETO FEITO COM A AJUDA DE FELIPE NEVES E DOUGLAS DROZDA
+
 function App() {
   return (
     <Switch>
