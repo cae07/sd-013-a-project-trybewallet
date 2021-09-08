f337e6128af34f2f7266914323c07049e69d48a4
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-09-08 08:45:19 -0300

    Adiciona requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Forms from '../components/forms';
 import Header from '../components/header';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <Forms />
+      </>
     );
   }
 }
