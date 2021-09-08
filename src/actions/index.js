ae61c1bfa54c76c763a27d0141c1dd66ab9888bd
Yuri <yurifranco18@live.com>
2021-09-08 12:45:05 -0300

    foi feito req. 7

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,38 @@
 export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const IS_FETCHING = 'IS_FETCHING';
+export const UPDATE_CURRENCIES = 'UPDATE_CURRENCIES';
+export const ERROR = 'ERROR';
 
 export const saveEmailAction = (email) => ({
   type: SAVE_EMAIL,
   payload: email,
 });
+
+export const getCurrencies = (payload) => ({
+  type: UPDATE_CURRENCIES,
+  payload,
+});
+
+export const isFetching = () => ({ type: IS_FETCHING });
+
+export const errorHandler = (payload) => ({
+  type: ERROR,
+  payload,
+});
+
+export const fetchCurrencies = () => (
+  async (dispatch) => {
+    dispatch(isFetching());
+    try {
+      const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+      if (!response.ok) throw new Error('Couldn\'t fetch data');
+      const data = await response.json();
+      delete data.USDT;
+      dispatch(getCurrencies(data));
+      dispatch(isFetching());
+    } catch (error) {
+      dispatch(errorHandler(error.message));
+      dispatch(isFetching());
+    }
+  }
+);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,10 +1,22 @@
+import { IS_FETCHING, UPDATE_CURRENCIES, ERROR } from '../actions';
+
 const INITIAL_STATE = {
+  isFetching: false,
+  error: '',
   currencies: [],
   expenses: [],
 };
 
 const walletReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case IS_FETCHING:
+    return { ...state, isFetching: !state.isFetching };
+
+  case UPDATE_CURRENCIES:
+    return { ...state, currencies: [action.payload] };
+
+  case ERROR:
+    return { ...state, error: action.payload };
   default:
     return state;
   }
