45f2761c909b2147b546fa14d0f3765b6b07c73d
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-09-08 08:34:11 -0300

    Adiciona requisito 5

--- src/components/rotas.js
+++ src/components/rotas.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import Login from '../pages/Login';
+import Wallet from '../pages/Wallet';
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
       </Switch>
     );
   }
